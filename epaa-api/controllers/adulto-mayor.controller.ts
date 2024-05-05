759932e4bd5b743e226f3b889b5b3f12236e211e
Nathan Hidalgo <nathan_hidalgo@hotmail.com>
2024-05-05 11:41:40 -0500

    basic endpoints am w/ test OK

--- epaa-api/controllers/adulto-mayor.controller.ts
+++ epaa-api/controllers/adulto-mayor.controller.ts
@@ -1,12 +1,17 @@
 import {
     Controller,
+    Get,
     Post,
+    Put,
+    Delete,
+    Param,
     Body,
     HttpStatus,
     HttpException,
 } from '@nestjs/common';
 import { AdultoMayorService } from '../services/adulto-mayor.service';
 import { CreateAdultoMayorDto } from 'dto/adulto-mayor/create-am.dto';
+import { UpdateAdultoMayorDto } from 'dto/adulto-mayor/update-am.dto';
 
 @Controller('adulto-mayor')
 export class AdultoMayorController {
@@ -15,8 +20,9 @@ export class AdultoMayorController {
     @Post()
     async create(@Body() createAdultoMayorDto: CreateAdultoMayorDto) {
         try {
-            const newAdultoMayor =
-                await this.adultoMayorService.create(createAdultoMayorDto);
+            const newAdultoMayor = await this.adultoMayorService.create(
+                createAdultoMayorDto,
+            );
             return {
                 message: 'Adulto mayor creado con éxito',
                 data: newAdultoMayor,
@@ -28,6 +34,87 @@ export class AdultoMayorController {
                     error: 'Error creating adulto mayor',
                 },
                 HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id: number,
+        @Body() updateAdultoMayorDto: UpdateAdultoMayorDto,
+    ) {
+        try {
+            const updateAdultoMayor = await this.adultoMayorService.update(
+                id,
+                updateAdultoMayorDto,
+            );
+            return {
+                message: 'Adulto mayor actualizado con éxito',
+                data: updateAdultoMayor,
+            };
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: 'Error updating adulto mayor',
+                },
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number) {
+        try {
+            await this.adultoMayorService.delete(id);
+            return {
+                message: 'Perfil eliminado con éxito',
+            };
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.INTERNAL_SERVER_ERROR,
+                    error: 'Error eliminando adulto mayor',
+                },
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: number) {
+        try {
+            const adultoMayor = await this.adultoMayorService.findById(id);
+            return {
+                message: 'Adulto mayor encontrado',
+                data: adultoMayor,
+            };
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    error: 'Adulto mayor no encontrado',
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
+    }
+
+    @Get()
+    async findAll() {
+        try {
+            const adultoMayor = await this.adultoMayorService.findAll();
+            return {
+                message: 'Adulto mayor encontrado',
+                data: adultoMayor,
+            };
+        } catch (error) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    error: 'Adulto mayor no encontrado',
+                },
+                HttpStatus.NOT_FOUND,
             );
         }
     }
--- epaa-api/test/adulto-mayor.service.spec.ts
+++ epaa-api/test/adulto-mayor.service.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
 import { AdultoMayorService } from '../services/adulto-mayor.service';
 import { AdultoMayor } from '../models/adulto_mayor.entity';
 import { CreateAdultoMayorDto } from '../dto/adulto-mayor/create-am.dto';
+import { UpdateAdultoMayorDto } from 'dto/adulto-mayor/update-am.dto';
 
 describe('AdultoMayorService', () => {
     let service: AdultoMayorService;
     let repositoryMock: any;
     let createAdultoMayorDto: CreateAdultoMayorDto;
+    let updateAdultoMayorDto: UpdateAdultoMayorDto;
 
     beforeEach(async () => {
         createAdultoMayorDto = {
@@ -18,9 +21,18 @@ describe('AdultoMayorService', () => {
             seguro: true,
         };
 
+        updateAdultoMayorDto = {
+            nombre: 'Jonh Suh',
+            seguro: false,
+        };
+
         repositoryMock = {
             create: jest.fn().mockReturnValue(createAdultoMayorDto),
-            save: jest.fn().mockReturnValue(createAdultoMayorDto),
+            save: jest.fn().mockResolvedValue(createAdultoMayorDto),
+            findOneById: jest.fn().mockResolvedValue(createAdultoMayorDto),
+            merge: jest.fn().mockReturnValue(updateAdultoMayorDto),
+            delete: jest.fn().mockResolvedValue({ affected: 1 }),
+            find: jest.fn().mockResolvedValue([createAdultoMayorDto]),
         };
 
         const module: TestingModule = await Test.createTestingModule({
@@ -45,6 +57,57 @@ describe('AdultoMayorService', () => {
             await expect(
                 service.create(createAdultoMayorDto),
             ).resolves.not.toThrow();
+        });
+    });
+
+    describe('update', () => {
+        it('should update an existing adulto mayor', async () => {
+            await expect(
+                service.update(1, updateAdultoMayorDto),
+            ).resolves.not.toThrow();
+        });
+
+        it('should throw a NotFoundException if no adulto mayor is found', async () => {
+            repositoryMock.findOneById.mockResolvedValueOnce(null);
+            await expect(
+                service.update(99, updateAdultoMayorDto),
+            ).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('delete', () => {
+        it('should delete an adulto mayor', async () => {
+            await expect(service.delete(1)).resolves.not.toThrow();
+        });
+
+        it('should throw a NotFoundException if no adulto mayor is deleted', async () => {
+            repositoryMock.delete.mockResolvedValueOnce({ affected: 0 });
+            await expect(service.delete(99)).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('findById', () => {
+        it('should return an adulto mayor by ID', async () => {
+            await expect(service.findById(1)).resolves.toBe(
+                createAdultoMayorDto,
+            );
+        });
+
+        it('should throw a NotFoundException if no adulto mayor is found', async () => {
+            repositoryMock.findOneById.mockResolvedValueOnce(null);
+            await expect(service.findById(99)).rejects.toThrow(
+                NotFoundException,
+            );
+        });
+    });
+
+    describe('findAll', () => {
+        it('should return all adultos mayores', async () => {
+            await expect(service.findAll()).resolves.toBeInstanceOf(Array);
+            await expect(service.findAll()).resolves.toHaveLength(1);
+            await expect(service.findAll()).resolves.toEqual([
+                createAdultoMayorDto,
+            ]);
         });
     });
 });
