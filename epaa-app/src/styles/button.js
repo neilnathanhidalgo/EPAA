02736a5ab75e2859c2f6fc6b4fdbecbd1bd13e71
Nathan Hidalgo <nathan_hidalgo@hotmail.com>
2024-05-09 23:45:05 -0500

    primera prueba

---
+++ epaa-app/src/styles/button.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  button: {
+    backgroundColor: "#FFA500",
+    borderRadius: 5,
+    alignItems: "center",
+  },
+
+  text: {
+    color: "white",
+    fontSize: 16,
+    padding: 10,
+  },
+});
